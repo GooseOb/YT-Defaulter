b0d7b1c083695ac25c37d77f5f111bbf56ec7768
GooseOb <gooose.ob@gmail.com>
2023-05-09 13:42:01 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ const {readFile} = require('fs/promises');
 const header = readFile('header.txt', 'utf-8');
 
 const typescript = async () => tsProject.src()
-    .pipe(tsProject()).js
-    .pipe(replace(/ {4}/g, '\t'))
-    .pipe(replace(/\n\t/g, '\n'))
-    .pipe(replace(/^/, await header))
-    .pipe(gulp.dest('.'));
+	.pipe(tsProject()).js
+	.pipe(replace(/ {4}/g, '\t'))
+	.pipe(replace(/\n\t/g, '\n'))
+	.pipe(replace(/^/, await header))
+	.pipe(gulp.dest('.'));
 
 exports.default = () => {
-    typescript();
-    gulp.watch('index.ts', typescript);
+	typescript();
+	gulp.watch('index.ts', typescript);
 };
