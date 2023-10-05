ab574cea3fedfed87696346abdc457f7d4cdc6de
GooseOb <gooose.ob@gmail.com>
2023-10-06 00:40:42 +0200

    small improvement in the build config

--- build-config.ts
+++ build-config.ts
@@ -3,9 +3,12 @@ import {BunBuildUserscriptConfig} from 'bun-build-userscript';
 
 const define: Record<string, string> = {};
 
-for (const [, key, value] of
-	(await readFile('index.ts', 'utf8')).match(/declare const.+?;/s)[0].matchAll(/(\S+): (\S+)[,;]/g)
-) define[key] = value;
+const raw = (await readFile('index.ts', 'utf8'))
+	.match(/declare const.+?;/s)[0];
+const pattern = /(\S+): (\S+)[,;]/g;
+
+let tmp;
+while (tmp = pattern.exec(raw)) define[tmp[1]] = tmp[2];
 
 export default {
 	define,
