ed78f3d2c3d6991847e9d94bd69d29b2ca9ec006
GooseOb <gooose.ob@gmail.com>
2023-11-10 19:04:30 +0100

    quality changing fix & some optimization

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Defaulter
 // @namespace    https://greasyfork.org/ru/users/901750-gooseob
-// @version      1.6.15
+// @version      1.6.16
 // @description  Set speed, quality, subtitles and volume as default globally or specialize for each channel
 // @author       GooseOb
 // @license      MIT
@@ -153,15 +153,41 @@ var getPlr = () => $("movie_player");
 var getAboveTheFold = () => $("above-the-fold");
 var getActionsBar = () => $("actions")?.querySelector("ytd-menu-renderer");
 var iconD = {
-  ["quality"]: "M6,14v-4c0-0.55,.45-1,1-1h3c0.55,0,1,.45,1,1v1H9.5v-0.5h-2v3h2V13H11v1c0,.55-0.45,1-1,1H7C6.45,15,6,14.55,6,14z            M14,15h3c0.55,0,1-0.45,1-1v-1h-1.5v0.5h-2v-3h2V11H18v-1c0-0.55-0.45-1-1-1h-3c-0.55,0-1,.45-1,1v4C13,14.55,13.45,15,14,15z            M20,4H4v16h16V4 M21,3v18H3V3.01C3,3,3,3,3.01,3H21L21,3z",
+  ["quality"]: "M15,17h6v1h-6V17z M11,17H3v1h8v2h1v-2v-1v-2h-1V17z M14,8h1V6V5V3h-1v2H3v1h11V8z            M18,5v1h3V5H18z M6,14h1v-2v-1V9H6v2H3v1 h3V14z M10,12h11v-1H10V12z",
   ["speed"]: "M10,8v8l6-4L10,8L10,8z M6.3,5L5.7,4.2C7.2,3,9,2.2,11,2l0.1,1C9.3,3.2,7.7,3.9,6.3,5z            M5,6.3L4.2,5.7C3,7.2,2.2,9,2,11 l1,.1C3.2,9.3,3.9,7.7,5,6.3z            M5,17.7c-1.1-1.4-1.8-3.1-2-4.8L2,13c0.2,2,1,3.8,2.2,5.4L5,17.7z            M11.1,21c-1.8-0.2-3.4-0.9-4.8-2 l-0.6,.8C7.2,21,9,21.8,11,22L11.1,21z            M22,12c0-5.2-3.9-9.4-9-10l-0.1,1c4.6,.5,8.1,4.3,8.1,9s-3.5,8.5-8.1,9l0.1,1 C18.2,21.5,22,17.2,22,12z"
 };
 var getYtElementFinderInArray = (elems) => (name) => elems.find((el) => el.querySelector(`path[d="${iconD[name]}"]`));
 var untilChannelUsernameAppear = () => untilAppear(getChannelUsername).catch(() => "");
-var isMusicChannel = async () => {
-  const el = await untilAppear(getAboveTheFold);
-  return !!el.querySelector(".badge-style-type-verified-artist");
-};
+var isMusicChannel = () => untilAppear(getAboveTheFold).then((el) => !!el.querySelector(".badge-style-type-verified-artist"));
+
+class YtMenu {
+  constructor(plr) {
+    this.element = plr.querySelector(".ytp-settings-menu");
+    this.btn = plr.querySelector(".ytp-settings-button");
+    restoreFocusAfter(() => {
+      this.btn.click();
+      this.btn.click();
+    });
+  }
+  element;
+  btn;
+  isOpen() {
+    return this.element.style.display !== "none";
+  }
+  open() {
+    if (!this.isOpen())
+      this.btn.click();
+  }
+  close() {
+    if (this.isOpen())
+      this.btn.click();
+  }
+  openItem(item) {
+    this.open();
+    item.click();
+    return this.element.querySelectorAll(".ytp-panel-animate-forward .ytp-menuitem-label");
+  }
+}
 var validateVolume = (value) => {
   const num = +value;
   return num < 0 || num > 100 ? "out of range" : isNaN(num) ? "not a number" : false;
@@ -188,7 +214,6 @@ var valueSetters = {
         btn.click();
         break;
       }
-    ytMenu.close();
   },
   speed(value) {
     this._ytSettingItem(isSpeedChanged ? SPEED_NORMAL : value, "speed");
@@ -251,30 +276,8 @@ var onPageChange = async () => {
   const getAd = () => plr.querySelector(".ytp-ad-player-overlay");
   if (getAd())
     await until(getAd, (ad) => !ad, 200000);
-  ytMenu = Object.assign(plr.querySelector(".ytp-settings-menu"), {
-    _btn: plr.querySelector(".ytp-settings-button"),
-    isOpen() {
-      return this.style.display !== "none";
-    },
-    open() {
-      if (!this.isOpen())
-        this._btn.click();
-    },
-    close() {
-      if (this.isOpen())
-        this._btn.click();
-    },
-    openItem(item) {
-      this.open();
-      item.click();
-      return this.querySelectorAll(".ytp-panel-animate-forward .ytp-menuitem-label");
-    }
-  });
-  restoreFocusAfter(() => {
-    ytMenu.open();
-    ytMenu.close();
-  });
-  const getMenuItems = () => ytMenu.querySelectorAll('.ytp-menuitem[role="menuitem"]');
+  ytMenu = new YtMenu(plr);
+  const getMenuItems = () => ytMenu.element.querySelectorAll('.ytp-menuitem[role="menuitem"]');
   const menuItemArr = Array.from(await until(getMenuItems, (arr) => !!arr.length));
   const getYtElement = getYtElementFinderInArray(menuItemArr);
   Object.assign(ytSettingItems, {
@@ -288,7 +291,6 @@ var onPageChange = async () => {
           SPEED_NORMAL = textContent;
           break;
         }
-      ytMenu.close();
     });
   const doNotChangeSpeed = cfg.flags.standardMusicSpeed && await isMusicChannel();
   const settings = {
@@ -314,11 +316,12 @@ var onPageChange = async () => {
   restoreFocusAfter(() => {
     for (const setting in settings)
       valueSetters[setting](settings[setting]);
+    if (!isNaN(+customSpeed)) {
+      isSpeedChanged = false;
+      valueSetters.customSpeed(customSpeed);
+    }
+    ytMenu.close();
   });
-  if (!isNaN(+customSpeed)) {
-    isSpeedChanged = false;
-    valueSetters.customSpeed(customSpeed);
-  }
   if (menu)
     return;
   const div = getElCreator("div"), input = getElCreator("input"), checkbox = (props) => input({ type: "checkbox", ...props }), option = getElCreator("option"), _label = getElCreator("label"), labelEl = (forId, props) => {
--- index.ts
+++ index.ts
@@ -235,7 +235,7 @@ const getActionsBar = () => $('actions')?.querySelector('ytd-menu-renderer');
 
 const iconD = {
 	[QUALITY]:
-		'M6,14v-4c0-0.55,.45-1,1-1h3c0.55,0,1,.45,1,1v1H9.5v-0.5h-2v3h2V13H11v1c0,.55-0.45,1-1,1H7C6.45,15,6,14.55,6,14z            M14,15h3c0.55,0,1-0.45,1-1v-1h-1.5v0.5h-2v-3h2V11H18v-1c0-0.55-0.45-1-1-1h-3c-0.55,0-1,.45-1,1v4C13,14.55,13.45,15,14,15z            M20,4H4v16h16V4 M21,3v18H3V3.01C3,3,3,3,3.01,3H21L21,3z',
+		'M15,17h6v1h-6V17z M11,17H3v1h8v2h1v-2v-1v-2h-1V17z M14,8h1V6V5V3h-1v2H3v1h11V8z            M18,5v1h3V5H18z M6,14h1v-2v-1V9H6v2H3v1 h3V14z M10,12h11v-1H10V12z',
 	[SPEED]:
 		'M10,8v8l6-4L10,8L10,8z M6.3,5L5.7,4.2C7.2,3,9,2.2,11,2l0.1,1C9.3,3.2,7.7,3.9,6.3,5z            M5,6.3L4.2,5.7C3,7.2,2.2,9,2,11 l1,.1C3.2,9.3,3.9,7.7,5,6.3z            M5,17.7c-1.1-1.4-1.8-3.1-2-4.8L2,13c0.2,2,1,3.8,2.2,5.4L5,17.7z            M11.1,21c-1.8-0.2-3.4-0.9-4.8-2 l-0.6,.8C7.2,21,9,21.8,11,22L11.1,21z            M22,12c0-5.2-3.9-9.4-9-10l-0.1,1c4.6,.5,8.1,4.3,8.1,9s-3.5,8.5-8.1,9l0.1,1 C18.2,21.5,22,17.2,22,12z',
 } as const;
@@ -247,19 +247,40 @@ const getYtElementFinderInArray =
 const untilChannelUsernameAppear = () =>
 	untilAppear(getChannelUsername).catch(() => '');
 
-const isMusicChannel = async () => {
-	const el = await untilAppear(getAboveTheFold);
-	return !!el.querySelector('.badge-style-type-verified-artist');
-};
+const isMusicChannel = () =>
+	untilAppear(getAboveTheFold).then(
+		(el) => !!el.querySelector('.badge-style-type-verified-artist')
+	);
 
-type YtMenuApi = {
-	_btn: HTMLButtonElement;
-	isOpen(): boolean;
-	open(): void;
-	close(): void;
-	openItem(item: HTMLElement): NodeListOf<HTMLElement>;
-};
-type YtMenu = HTMLElement & YtMenuApi;
+class YtMenu {
+	constructor(plr: HTMLElement) {
+		this.element = plr.querySelector('.ytp-settings-menu');
+		this.btn = plr.querySelector('.ytp-settings-button');
+		restoreFocusAfter(() => {
+			this.btn.click();
+			this.btn.click();
+		});
+	}
+	element: HTMLElement;
+	private btn: HTMLElement;
+	private isOpen() {
+		return this.element.style.display !== 'none';
+	}
+	open() {
+		if (!this.isOpen()) this.btn.click();
+	}
+	close() {
+		if (this.isOpen()) this.btn.click();
+	}
+	openItem(item: YtSettingItem) {
+		this.open();
+		item.click();
+		return this.element.querySelectorAll<HTMLElement>(
+			'.ytp-panel-animate-forward .ytp-menuitem-label'
+		);
+	}
+}
+
 type YtSettingName = typeof SPEED | typeof QUALITY;
 type YtSettingItem = HTMLDivElement & { role: 'menuitem' };
 type YtSettingItems = Partial<Record<YtSettingName, YtSettingItem>>;
@@ -307,7 +328,6 @@ const valueSetters: ValueSetters & ValueSetterHelpers = {
 				btn.click();
 				break;
 			}
-		ytMenu.close();
 	},
 	speed(value) {
 		this._ytSettingItem(isSpeedChanged ? SPEED_NORMAL : value, SPEED);
@@ -370,31 +390,11 @@ const onPageChange = async () => {
 	await delay(1_000);
 	const getAd = () => plr.querySelector('.ytp-ad-player-overlay');
 	if (getAd()) await until(getAd, (ad) => !ad, 200_000);
-	ytMenu = Object.assign(plr.querySelector<HTMLElement>('.ytp-settings-menu'), {
-		_btn: plr.querySelector('.ytp-settings-button'),
-		isOpen() {
-			return this.style.display !== 'none';
-		},
-		open() {
-			if (!this.isOpen()) this._btn.click();
-		},
-		close() {
-			if (this.isOpen()) this._btn.click();
-		},
-		openItem(item) {
-			this.open();
-			item.click();
-			return this.querySelectorAll(
-				'.ytp-panel-animate-forward .ytp-menuitem-label'
-			);
-		},
-	} satisfies YtMenuApi);
-	restoreFocusAfter(() => {
-		ytMenu.open();
-		ytMenu.close();
-	});
+	ytMenu = new YtMenu(plr);
 	const getMenuItems = () =>
-		ytMenu.querySelectorAll<YtSettingItem>('.ytp-menuitem[role="menuitem"]');
+		ytMenu.element.querySelectorAll<YtSettingItem>(
+			'.ytp-menuitem[role="menuitem"]'
+		);
 	const menuItemArr = Array.from(
 		await until(getMenuItems, (arr) => !!arr.length)
 	);
@@ -410,7 +410,6 @@ const onPageChange = async () => {
 					SPEED_NORMAL = textContent;
 					break;
 				}
-			ytMenu.close();
 		});
 	const doNotChangeSpeed =
 		cfg.flags.standardMusicSpeed && (await isMusicChannel());
@@ -436,11 +435,12 @@ const onPageChange = async () => {
 	restoreFocusAfter(() => {
 		for (const setting in settings)
 			valueSetters[setting as Setting](settings[setting as never]);
+		if (!isNaN(+customSpeed)) {
+			isSpeedChanged = false;
+			valueSetters.customSpeed(customSpeed);
+		}
+		ytMenu.close();
 	});
-	if (!isNaN(+customSpeed)) {
-		isSpeedChanged = false;
-		valueSetters.customSpeed(customSpeed);
-	}
 
 	/* ---------------------- settings menu ---------------------- */
 
