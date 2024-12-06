5e62c433c53f658fb70844eaec53882690692516
GooseOb <gooose.ob@gmail.com>
2024-12-06 02:50:34 +0100

    make promise to reject an error

--- utils/until.ts
+++ utils/until.ts
@@ -16,7 +16,7 @@ export const until = <T>(
 				res(item);
 			} else if (++i > reqLimit) {
 				clearInterval(interval);
-				rej("Timeout: item wasn't found");
+				rej(new Error("Timeout: item wasn't found"));
 			}
 		}, msReqTimeout);
 	});
