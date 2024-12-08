4809aa24449dc43564bc1127532c301e56cd661c
GooseOb <gooose.ob@gmail.com>
2024-12-08 04:23:01 +0100

    move functions that do not interact with UI out of `restoreFocusAfter` callback

--- src/player/apply-settings.ts
+++ src/player/apply-settings.ts
@@ -3,13 +3,13 @@ import { plr } from './plr';
 import { valueSetters } from './value-setters';
 
 export const applySettings = (settings: Cfg) => {
+	if (!isNaN(+settings.customSpeed)) {
+		valueSetters.customSpeed(settings.customSpeed);
+	}
+
+	delete settings.customSpeed;
+
 	restoreFocusAfter(() => {
-		if (!isNaN(+settings.customSpeed)) {
-			valueSetters.customSpeed(settings.customSpeed);
-		}
-
-		delete settings.customSpeed;
-
 		for (const setting in settings) {
 			valueSetters[setting as Setting](settings[setting as never]);
 		}
