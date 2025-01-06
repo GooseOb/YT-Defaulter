ef024c7540d2973e7d7e733d4f20664776ec5a75
GooseOb <gooose.ob@gmail.com>
2025-01-06 01:31:21 +0100

    [skip ci] update bundler package

--- build-config.ts
+++ build-config.ts
@@ -1,26 +1,21 @@
 import { readFile } from 'node:fs/promises';
-import type { BuildUserscriptConfig } from 'bun-build-userscript';
 
-const define: Record<string, string> = {};
-
-const raw = await readFile('src/constants.ts', 'utf8');
 const pattern = /([A-Z_0-9]+):\s*('[^']+'|[^,;]+)/g;
 
-let tmp: RegExpExecArray | null;
-while ((tmp = pattern.exec(raw))) {
-	define[tmp[1]] = tmp[2];
-}
+export const bun = {
+	outdir: 'dist',
+};
 
-export default {
-	// @ts-expect-error
-	entrypoints: ['src/index.ts'],
-	naming: 'dist/script.user.js',
-	define,
-	userscript: {
-		logErrors: process.argv.includes('--watch'),
-		transform: (code: string) =>
-			code
-				.replace(/\["([^"]+)"]:/g, '$1:')
-				.replace(/(\S)\["([^"]+)"]/g, '$1.$2'),
+export const userscript = {
+	before: async ({ bun }) => {
+		bun.define = {};
+		const raw = await readFile('src/constants.ts', 'utf8');
+		let tmp: RegExpExecArray | null;
+		while ((tmp = pattern.exec(raw))) {
+			bun.define[tmp[1]] = tmp[2];
+		}
 	},
-} satisfies BuildUserscriptConfig;
+	entry: 'src',
+	transform: (code) =>
+		code.replace(/\["([^"]+)"]:/g, '$1:').replace(/(\S)\["([^"]+)"]/g, '$1.$2'),
+};
