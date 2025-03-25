8d021962cc199fa617c20aa12949502fa6d556a9
GooseOb <gooose.ob@gmail.com>
2025-03-25 22:14:36 +0100

    add a migration for a flag name

--- src/config/update.ts
+++ src/config/update.ts
@@ -4,23 +4,29 @@ import type { ScriptCfg } from './types';
  *  @returns if the cfg was updated
  */
 export const update = (cfg: ScriptCfg) => {
-	const doUpdate = cfg._v !== STORAGE_VERSION;
-	if (doUpdate) {
-		switch (cfg._v) {
-			case 2:
-				cfg.flags.standardMusicSpeed = false;
-				cfg._v = 3;
-			// fall through
-			case 3:
-				cfg.global.quality = (cfg.global as any).qualityMax;
-				delete (cfg.global as any).qualityMax;
-				for (const key in cfg.channels) {
-					const currCfg = cfg.channels[key];
-					currCfg.quality = (currCfg as any).qualityMax;
-					delete (currCfg as any).qualityMax;
-				}
-				cfg._v = STORAGE_VERSION;
-		}
+	let isUpdated = true;
+	switch (cfg._v) {
+		case STORAGE_VERSION:
+			if ('shortsToUsual' in cfg.flags) {
+				cfg.flags.shortsToRegular = cfg.flags.shortsToUsual;
+				delete cfg.flags.shortsToUsual;
+			} else {
+				isUpdated = false;
+			}
+			break;
+		case 2:
+			cfg.flags.standardMusicSpeed = false;
+			cfg._v = 3;
+		// fall through
+		case 3:
+			cfg.global.quality = (cfg.global as any).qualityMax;
+			delete (cfg.global as any).qualityMax;
+			for (const key in cfg.channels) {
+				const currCfg = cfg.channels[key];
+				currCfg.quality = (currCfg as any).qualityMax;
+				delete (currCfg as any).qualityMax;
+			}
+			cfg._v = STORAGE_VERSION;
 	}
-	return doUpdate;
+	return isUpdated;
 };
